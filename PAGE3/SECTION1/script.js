c69d88e9aaca084006a74d61dd56dcd0ec742990
bigdeal100x <estelleh@ocadu.ca>
2025-10-22 01:18:45 -0400

    adding files

---
+++ PAGE3/SECTION1/script.js
@@ -0,0 +1,11 @@
+$("#button").click(function(){
+    $(".transform").toggleClass("transform-active");
+ });
+
+ $("#button2").click(function(){
+    $(".transform2").toggleClass("transform2-active");
+ });
+
+ $("#button3").click(function(){
+    $(".transform3").toggleClass("transform3-active");
+ });
